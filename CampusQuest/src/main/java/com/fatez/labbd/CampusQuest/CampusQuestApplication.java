fdbcb644c336a89c85a5827664d677466b047ca2
LuquinhasDoJava <143123619+LuquinhasDoJava@users.noreply.github.com>
2025-10-13 22:51:12 -0300

    Add files via upload

---
+++ CampusQuest/src/main/java/com/fatez/labbd/CampusQuest/CampusQuestApplication.java
@@ -0,0 +1,13 @@
+package com.fatez.labbd.CampusQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusQuestApplication.class, args);
+	}
+
+}
---
+++ CampusQuest/src/test/java/com/fatez/labbd/CampusQuest/CampusQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatez.labbd.CampusQuest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
